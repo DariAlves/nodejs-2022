0bbd6eea1f67ac7c1e592f67044a46c182d7af0c
Dari Alves <darieltondf@gmail.com>
2022-03-28 23:08:48 -0300

    variaveis let

---
+++ nodejs-001/001/02-variaveis-let.js
@@ -0,0 +1,27 @@
+// Variáveis - Let
+
+console.log(`01-não faz hoisting\n`);
+// console.log(`01-hoisting: ${raca}\n`); ReferenceError: Cannot access 'raca' before initialization
+
+let raca = 'Saiyajin';
+
+console.log(`02-declaração e atribuição: ${raca}\n`);
+
+function dbz() {
+  let raca = 'Namekuseijin';
+
+  console.log(`03-escopo de função: ${raca}\n`);
+}
+
+dbz();
+
+console.log(`04-valor permanece inalterado: ${raca}\n`);
+
+{
+  // Respeira o escopo de bloco
+  let raca = 'Majin';
+  console.log(`05-escopo de bloco: ${raca}\n`);
+}
+
+// Valor permanece o mesmo do inicial, porque não foi alterado
+console.log(`06-Não acessível fora do bloco: ${raca}\n`);
