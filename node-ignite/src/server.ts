e0ad90c9f3f4bb74610df80d4af365fcb8961aaf
Dari Alves <darieltondf@gmail.com>
2022-05-02 19:31:03 -0300

    :sunglasses: debug

--- node-ignite/src/server.ts
+++ node-ignite/src/server.ts
@@ -4,8 +4,16 @@ const PORT = 3333;
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, response) => {
   return response.json({ message: 'Node.js' });
+});
+
+app.post('/courses', (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name });
 });
 
 app.listen(PORT, () => {
