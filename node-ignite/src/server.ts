b8a1bb0d2f3e62d287798dc8803fb95a70e4caa4
Dari Alves <darieltondf@gmail.com>
2022-05-04 21:05:32 -0300

    :sunglasses: add uuid

--- node-ignite/src/server.ts
+++ node-ignite/src/server.ts
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(categoriesRoutes);
+app.use('/categories', categoriesRoutes);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT} 🚀`);
