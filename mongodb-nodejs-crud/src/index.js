4b40b8b5737b3cd932bc3a051b57c9d538270668
Dari Alves <darieltondf@gmail.com>
2022-05-09 14:55:11 -0300

    :sunglasses: crud - read

--- mongodb-nodejs-crud/src/index.js
+++ mongodb-nodejs-crud/src/index.js
@@ -10,7 +10,10 @@ async function main() {
   try {
     await client.connect();
 
-    console.log('Connected successfully to mongodb server!');
+    console.log('Connected successfully to mongodb server!\n');
+
+    // Retrieve datas
+    await listAllSuperheroes(client);
   } catch (error) {
     console.error(error);
   } finally {
@@ -19,3 +22,16 @@ async function main() {
 }
 
 main().catch(console.error);
+
+// ! CRUD
+
+// Read
+async function listAllSuperheroes(client) {
+  const db = await client.db('superheroesAPI');
+
+  const allSuperheroes = await db.collection('superheroes').find().toArray();
+
+  allSuperheroes.forEach(superhero => {
+    console.log(`Name: ${superhero['name']}\nAlias: ${superhero['alias']}\n`);
+  });
+}
