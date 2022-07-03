748c508d3930bd4adaa4eea346d37abc31ad06a2
Dari Alves <darieltondf@gmail.com>
2022-07-03 00:37:23 -0300

    :sunglasses: criar categoria

--- node-typescript/rentalx/src/server.ts
+++ node-typescript/rentalx/src/server.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use(categoriesRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
