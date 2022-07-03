5bc8181acf4e6d4e1f5f6b869e2802630a82aabd
Dari Alves <darieltondf@gmail.com>
2022-07-03 01:25:42 -0300

    :sunglasses: tipagem categoria

--- node-typescript/rentalx/src/routes/categories.routes.ts
+++ node-typescript/rentalx/src/routes/categories.routes.ts
@@ -1,18 +1,16 @@
 import { Router } from 'express';
-import { v4 as uuidv4 } from 'uuid';
+import { Category } from '../models/Category';
 const categoriesRoutes = Router();
 
 // Database
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const category = {
-    id: uuidv4(),
-    name,
-    description,
-  };
+  const category = new Category();
+
+  Object.assign(category, { name, description });
 
   categories.push(category);
 
